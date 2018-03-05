d832de5c9068a407a4b30adb1179fe47f058aa68
Bronson Quick <bronson@bronsonquick.com.au>
2018-03-05 18:09:35 +1000

    Align arrows

--- modules/bower/manifests/init.pp
+++ modules/bower/manifests/init.pp
@@ -3,9 +3,9 @@ class bower (
 	$path = "/vagrant/extensions/bower",
 ) {
 	exec { 'install bower':
-		path        => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-		command     => 'npm install -g bower',
-		require     => [ Class['nodejs'] ],
+		path    => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
+		command => 'npm install -g bower',
+		require => [ Class['nodejs'] ],
 		unless  => 'which bower',
 	}
 }
